e0fd4c933a9555f5b881ceb0e3cea4c9624df9dc
James Wyrick <WyrickJM19@pwcs-edu.org>
2019-05-07 12:28:38 -0400

    Added .java file

---
+++ Doopitydoop.java
@@ -0,0 +1,17 @@
+package packag;
+import java.util.scanner;
+public class Doopitydoop {
+
+    
+    public void main() {
+        Scanner in = new Scanner(System.out);
+        System.out.println("Enter your name");
+        String name = in.nextLine();
+        String eman = "";
+        for(int i = name.length() - 1; i = 0; i++) {
+           name += eman.charAt(i);
+        }
+        System.out.println(eman);        
+    }
+    
+}
