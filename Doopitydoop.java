fcb2c02c883087e4ea0a0f885690fc0194f355f0
Mason Tincher <TincheMA19@pwcs-edu.org>
2019-05-21 12:21:45 -0400

    fixed code

--- Doopitydoop.java
+++ Doopitydoop.java
@@ -2,14 +2,16 @@ package packag;
 import java.util.scanner;
 public class Doopitydoop {
 
-    
-    public void main() {
+    //added static and put in a parameter
+    public static void main( String[] args ) {
         Scanner in = new Scanner(System.out);
         System.out.println("Enter your name");
         String name = in.nextLine();
         String eman = "";
-        for(int i = name.length() - 1; i = 0; i++) {
-           name += eman.charAt(i);
+	//changed to "i--" and "i >= 0"
+        for(int i = name.length() - 1; i >= 0; i--) {
+	   //switched eman and name
+           eman += name.charAt(i);
         }
         System.out.println(eman);        
     }
